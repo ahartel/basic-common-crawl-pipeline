7064d9b11205263572459d639a093e716ce2774b
Andreas Hartel <andreas.hartel@aleph-alpha.com>
2024-08-13 14:17:34 +0200

    can parse cluster idx file

--- pipeline/src/bin/batcher.rs
+++ pipeline/src/bin/batcher.rs
@@ -1,3 +1,4 @@
+use core::time;
 use std::{io::Read, time::Duration};
 
 use anyhow::Context;
@@ -161,9 +162,30 @@ fn parse_cdx_line(line: &str) -> CdxEntry {
     }
 }
 
+struct ClusterIdxEntry {
+    surt_url: String,
+    timestamp: String,
+    cdx_filename: String,
+    cdx_offset: usize,
+    cdx_length: usize,
+    cluster_id: String,
+}
+
+fn parse_cluster_idx(line: &str) -> ClusterIdxEntry {
+    let mut idx = line.split_whitespace();
+    ClusterIdxEntry {
+        surt_url: idx.next().unwrap().to_string(),
+        timestamp: idx.next().unwrap().to_string(),
+        cdx_filename: idx.next().unwrap().to_string(),
+        cdx_offset: idx.next().unwrap().parse().unwrap(),
+        cdx_length: idx.next().unwrap().parse().unwrap(),
+        cluster_id: idx.next().unwrap().to_string(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::parse_cdx_line;
+    use crate::{parse_cdx_line, parse_cluster_idx};
 
     #[test]
     fn can_parse_cdx_file() {
@@ -172,5 +194,15 @@ mod tests {
 0,100,59,139)/ 20240723213521 {"url": "https://139.59.100.0/", "mime": "text/html", "mime-detected": "text/html", "status": "200", "digest": "5JOQMMSNM6N7UCLGGYXDSPSB3FYAQS2C", "length": "16650", "offset": "64016172", "filename": "crawl-data/CC-MAIN-2024-30/segments/1720763518115.82/warc/CC-MAIN-20240723194208-20240723224208-00279.warc.gz", "charset": "UTF-8", "languages": "ind,eng"}"#;
         let cdx: Vec<_> = content.lines().map(parse_cdx_line).collect();
         assert_eq!(cdx.len(), 3);
+    }
+
+    #[test]
+    fn can_parse_cluster_idx_file() {
+        let content = r#"0,100,22,165)/ 20240722120756   cdx-00000.gz    0       188224  1
+101,141,199,66)/robots.txt 20240714155331       cdx-00000.gz    188224  178351  2
+104,223,1,100)/ 20240714230020  cdx-00000.gz    366575  178055  3
+107,128,254,23)/sites.asp?domain=hydrogenheaters.com 20240725183414     cdx-00000.gz    544630  181599  4"#;
+        let cdx_parts: Vec<_> = content.lines().map(parse_cluster_idx).collect();
+        assert_eq!(cdx_parts.len(), 4);
     }
 }
